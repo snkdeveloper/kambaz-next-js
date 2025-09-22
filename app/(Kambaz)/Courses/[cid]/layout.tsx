933e0e220c47f9a76ec08a25c3e8f87dab00e5d8
Sachet Kanchugar <sachetkanchugar@Sachets-MacBook-Air.local>
2025-09-22 03:36:06 -0400

    seventh commit

--- app/(Kambaz)/Courses/[cid]/layout.tsx
+++ app/(Kambaz)/Courses/[cid]/layout.tsx
@@ -1,30 +1,20 @@
 import { ReactNode } from "react";
 import CourseNavigation from "./Navigation";
+export default async function CoursesLayout(
+  { children, params }: Readonly<{ children: ReactNode; params: Promise<{ id: string }> }>) {
+ const { cid } = await params;
+ return (
+   <div id="wd-courses">
+     <h2>Courses {cid}</h2>
+     <hr />
+     <table>
+       <tbody>
+         <tr>
+           <td valign="top" width="200"> <CourseNavigation /> </td>
+           <td valign="top" width="100%"> {children} </td>
+         </tr>
+       </tbody>
+     </table>
+   </div>
+);}
 
-// ✅ Use Next.js built-in type for params
-export default function CoursesLayout({
-  children,
-  params,
-}: {
-  children: ReactNode;
-  params: { cid: string };
-}) {
-  return (
-    <div id="wd-courses">
-      <h2>Courses {params.cid}</h2>
-      <hr />
-      <table>
-        <tbody>
-          <tr>
-            <td valign="top" width="200">
-              <CourseNavigation />
-            </td>
-            <td valign="top" width="100%">
-              {children}
-            </td>
-          </tr>
-        </tbody>
-      </table>
-    </div>
-  );
-}
--- app/(Kambaz)/Courses/[cid]/page.tsx
+++ app/(Kambaz)/Courses/[cid]/page.tsx
@@ -1,10 +1,8 @@
 import { redirect } from "next/navigation";
 
-export default function CoursesPage({
-  params,
-}: {
-  params: { cid: string };
-}) {
-  const { cid } = params;
-  redirect(`/Courses/${cid}/Home`);
+
+export default async function CoursesPage({ params, }: { params: Promise<{ cid: string }>; }) {
+ const { cid } = await params;
+ redirect(`/Courses/${cid}/Home`);
 }
+
