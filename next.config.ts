361b5c157a23c3325c757f4d82339a2402b33a0b
Sachet Kanchugar <sachetkanchugar@Sachets-MacBook-Air.local>
2025-10-20 21:44:22 -0400

    a3 JavaScript

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  eslint: {
+    // 🚫 Don't run ESLint during builds (Vercel won't block deploys)
+    ignoreDuringBuilds: true,
+  },
   /* config options here */
 };
 
